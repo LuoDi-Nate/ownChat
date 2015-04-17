05daa66cd4071e0dbe12102833d51db6d5542e8b
LuoDi-Nate <543667537@qq.com>
2015-04-18 00:33:18 +0800

    coding

---
+++ src/main/java/com/diwa/chatServer/model/User.java
@@ -0,0 +1,12 @@
+package com.diwa.chatServer.model;
+
+/**
+ * Created by di on 18/4/15.
+ */
+public class User {
+    private int id;
+    private String nickName;
+    private String ip;
+    private int port;
+    
+}
---
+++ src/main/java/com/diwa/chatServer/serverMain/ChatServer.java
@@ -0,0 +1,13 @@
+package com.diwa.chatServer.serverMain;
+
+        import java.util.logging.Logger;
+
+/**
+ * Created by di on 6/4/15.
+ */
+public class ChatServer {
+    public static void main(String[] args) {
+
+    }
+}
+
