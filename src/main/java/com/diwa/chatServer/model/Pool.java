67e994e2517f241d5f1ed378e593a4d472f5a58f
LuoDi-Nate <543667537@qq.com>
2015-04-21 10:40:36 +0800

    cooding

--- src/main/java/com/diwa/chatServer/model/Pool.java
+++ src/main/java/com/diwa/chatServer/model/Pool.java
@@ -4,24 +4,28 @@ import java.util.List;
 
 /**
  * Created by di on 18/4/15.
+ *  维护一个池子,保持在线人员在这中间,每隔5分钟,踢出没有响应的旧用户
+ *  如果用户主动logout 也踢出池子
+ *  有可能在建立一个隐私池.或者在池子中有状态
+ *  一个server开启,只能有一个池子.
  */
 public class Pool {
-    private int count;
-    private List<User> onlineList;
+    private static int count;
+    private static List<User> onlineList;
 
-    public int getCount() {
+    public static int getCount() {
         return count;
     }
 
-    public void setCount(int count) {
-        this.count = count;
+    public static void setCount(int count) {
+        Pool.count = count;
     }
 
-    public List<User> getOnlineList() {
+    public static List<User> getOnlineList() {
         return onlineList;
     }
 
-    public void setOnlineList(List<User> onlineList) {
-        this.onlineList = onlineList;
+    public static void setOnlineList(List<User> onlineList) {
+        Pool.onlineList = onlineList;
     }
 }
---
+++ src/main/java/com/diwa/chatServer/service/LoginService.java
@@ -0,0 +1,8 @@
+package com.diwa.chatServer.service;
+
+/**
+ * Created by di on 21/4/15.
+ */
+public class LoginService {
+
+}
