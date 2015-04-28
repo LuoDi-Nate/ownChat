b5f82059d540ce19aec39b84efa1026cde47e1a1
LuoDi-Nate <543667537@qq.com>
2015-04-29 00:15:33 +0800

    feature/flashconsole done

--- src/main/java/com/diwa/chatServer/view/ServerView.java
+++ src/main/java/com/diwa/chatServer/view/ServerView.java
@@ -2,6 +2,8 @@ package com.diwa.chatServer.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by di on 28/4/15.
@@ -37,35 +39,35 @@ public class ServerView extends JFrame {
     private JButton exportBtn;
     private JButton exitBtn;
 
-    public ServerView(int port){
+    public ServerView(int port) {
         this.port = port;
         cc = getContentPane();
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setResizable(false);
         setSize(600, 360);
         setTitle("ownChatServer    _v1.0");
-        setLocation(400,200);
+        setLocation(400, 200);
         init();
     }
 
     //构造界面
-    private void init(){
+    private void init() {
         //north
         northPanel = new JPanel();
-        northPanel.setLayout(new GridLayout(1,3));
+        northPanel.setLayout(new GridLayout(1, 3));
         topLabel = new JLabel("ownChatServer_         ");
-        topLabel.setFont(new Font("楷体",Font.BOLD,25));
+        topLabel.setFont(new Font("楷体", Font.BOLD, 25));
         topVersionLabel = new JLabel("             author:diwa");
         northPanel.setLayout(new FlowLayout());
         northPanel.add(topLabel);
-        northPanel.add(new JLabel("DefaultPort:["+port+"]"));
+        northPanel.add(new JLabel("DefaultPort:[" + port + "]"));
         northPanel.add(topVersionLabel);
 
         //mid
         midPanel = new JPanel();
         consoleText = new JTextArea(consoleStr);
         eventText = new JTextArea(eventStr);
-        midPanel.setLayout(new GridLayout(1,2));
+        midPanel.setLayout(new GridLayout(1, 2));
         midPanel.add(new JScrollPane(consoleText));
         consoleText.setWrapStyleWord(true);
         consoleText.setEnabled(false);
@@ -77,7 +79,7 @@ public class ServerView extends JFrame {
         sourthPanel = new JPanel();
         startBtn = new JButton("START");
         statusBtn = new JButton(status);
-        statusBtn.setFont(new Font("楷体",Font.BOLD,20));
+        statusBtn.setFont(new Font("楷体", Font.BOLD, 25));
         statusBtn.setBackground(Color);
         stopBtn = new JButton("STOP");
         exportBtn = new JButton("export");
@@ -94,7 +96,25 @@ public class ServerView extends JFrame {
         cc.add(midPanel, BorderLayout.CENTER);
         cc.add(sourthPanel, BorderLayout.SOUTH);
 
+        //按键监听
+        actionInit();
+    }
 
+    //添加监听
+    private void actionInit() {
+        //退出键
+        exitBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                System.exit(0);
+            }
+        });
+
+        //start键添加动作
+        startBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -102,20 +122,39 @@ public class ServerView extends JFrame {
         sv.setVisible(true);
     }
 
-    public void flashServerView(int status, String newConsoleStr, String newEventStr){
-        switch (status){
-            case 0:this.status = "ON";this.Color = Color.green;break;
-            case 1:this.status = "OFF"; break;
+    //三个同名方法,重载,刷新界面用
+    public void flashServerView(int status, String newConsoleStr, String newEventStr) {
+        switch (status) {
+            case 0:
+                this.status = "ON";
+                this.Color = Color.green;
+                break;
+            case 1:
+                this.status = "OFF";
+                break;
         }
         this.statusBtn.setText(this.status);
         this.statusBtn.setBackground(this.Color);
 
         String oldConsole = consoleText.getText();
-        this.consoleText.setText(oldConsole+"\n"+newConsoleStr);
+        this.consoleText.setText(oldConsole + "\n" + newConsoleStr);
 
         String oldEventStr = eventText.getText();
-        this.eventText.setText(oldEventStr+"\n"+newEventStr);
+        this.eventText.setText(oldEventStr + "\n" + newEventStr);
 
+    }
+
+    public void flashServerView(String newConsoleStr, String newEventStr) {
+        String oldConsole = consoleText.getText();
+        this.consoleText.setText(oldConsole + "\n" + newConsoleStr);
+
+        String oldEventStr = eventText.getText();
+        this.eventText.setText(oldEventStr + "\n" + newEventStr);
+    }
+
+    public void flashServerView(String newConsoleStr) {
+        String oldConsole = consoleText.getText();
+        this.consoleText.setText(oldConsole + "\n" + newConsoleStr);
     }
 
 
